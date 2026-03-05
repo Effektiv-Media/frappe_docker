fcbc325f4541b65ddebe1af0accc6fb2130cc165
01maxpoh <01maxpoh@gafe.molndal.se>
2026-03-05 12:55:48 +0100

    Rebuilding

--- startup.sh
+++ startup.sh
@@ -20,19 +20,59 @@ if [ "$SITE_EXISTS" -eq 0 ]; then
   bench --site signello install-app signello_2
 fi
 
-# Always run these on every boot
+# Always run on every boot
 bench use signello
 echo "signello" > sites/currentsite.txt
 echo '{"serve_default_site": true, "default_site": "signello"}' > sites/common_site_config.json
 bench --site signello set-config host_name "https://$HOST_NAME"
 ln -sf /home/frappe/frappe-bench/sites/signello /home/frappe/frappe-bench/sites/$HOST_NAME
 
-# Run migrations in case of new code
 bench --site signello migrate
+bench --site signello build --force
 
+# Write nginx config
+cat > /etc/nginx/conf.d/frappe.conf << 'EOF'
+upstream frappe {
+    server 127.0.0.1:8000;
+}
+
+server {
+    listen 80;
+    server_name _;
+
+    root /home/frappe/frappe-bench/sites;
+
+    location /assets {
+        try_files $uri =404;
+    }
+
+    location ~ ^/files/.*.(htm|html|svg|xml) {
+        add_header Content-disposition "attachment";
+        try_files $uri =404;
+    }
+
+    location /files {
+        try_files $uri =404;
+    }
+
+    location / {
+        proxy_pass http://frappe;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_read_timeout 120;
+    }
+}
+EOF
+
+# Start nginx in background
+nginx -g 'daemon off;' &
+
+# Start gunicorn in foreground
 exec /home/frappe/frappe-bench/env/bin/gunicorn \
   --chdir=/home/frappe/frappe-bench/sites \
-  --bind=0.0.0.0:8000 \
+  --bind=127.0.0.1:8000 \
   --threads=4 \
   --workers=2 \
   --worker-class=gthread \
