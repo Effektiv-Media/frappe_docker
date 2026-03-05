6e3d4f87e8cb609b1539051062273f801dffbe4f
01maxpoh <01maxpoh@gafe.molndal.se>
2026-03-05 10:05:15 +0100

    Rebuilding

--- startup.sh
+++ startup.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-set -e
 
 cd /home/frappe/frappe-bench
 
-if [ ! -d "sites/signello" ]; then
-  echo "Creating site..."
-  bench new-site signello \
-    --mariadb-root-password "$MYSQL_ROOT_PASSWORD" \
-    --admin-password "$MYSQL_ROOT_PASSWORD" \
-    --db-host "$MYSQLHOST" \
-    --db-port 3306 \
-    --mariadb-user-host-login-scope='%'
+bench new-site signello \
+  --mariadb-root-password "$MYSQL_ROOT_PASSWORD" \
+  --admin-password "$MYSQL_ROOT_PASSWORD" \
+  --db-host "$MYSQLHOST" \
+  --db-port 3306 \
+  --mariadb-user-host-login-scope='%' \
+  --force
 
-  bench --site signello install-app erpnext
-  bench --site signello install-app signello_2
-  bench use signello
-  bench --site signello set-config host_name "$HOST_NAME"
-  bench set-config -g serve_default_site true
-fi
+bench --site signello install-app erpnext
+bench --site signello install-app signello_2
+bench use signello
+bench --site signello set-config host_name "$HOST_NAME"
+echo '{"serve_default_site": true}' > sites/common_site_config.json
 
 exec /home/frappe/frappe-bench/env/bin/gunicorn \
   --chdir=/home/frappe/frappe-bench/sites \
